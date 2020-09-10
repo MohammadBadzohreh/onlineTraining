524720151f55eb5ed98d9dffe7f2618f4bc90fe0
Mohammad Badzohreh <badzohreee@gmail.com>
2020-09-10 22:55:34 +0430

    =added dashboard routes

--- moduls/Badzohreh/Dashboard/Routes/dashboard_routes.php
+++ moduls/Badzohreh/Dashboard/Routes/dashboard_routes.php
@@ -1,6 +1,7 @@
 <?php
 
-Route::group(['namespace'=>'Badzohreh\Dashboard\Http\Controllers'],function ($router){
+Route::group(['namespace' => 'Badzohreh\Dashboard\Http\Controllers',
+    'middleware' => ['web','auth', 'verified']], function ($router) {
 
     $router->get('/home', 'DashboardController@index')->name('home');
 });
