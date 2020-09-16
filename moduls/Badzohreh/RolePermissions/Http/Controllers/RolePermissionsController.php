c35b719eb1bfb7c8d06066a389c9fc86eaad6649
Mohammad Badzohreh <badzohreee@gmail.com>
2020-09-16 18:40:15 +0430

    =add update permissions

--- moduls/Badzohreh/RolePermissions/Http/Controllers/RolePermissionsController.php
+++ moduls/Badzohreh/RolePermissions/Http/Controllers/RolePermissionsController.php
@@ -3,19 +3,49 @@
 namespace Badzohreh\RolePermissions\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
+use Badzohreh\RolePermissions\Http\Requests\RolePermissionStoreRequest;
+use Badzohreh\RolePermissions\http\Requests\UpdateRolerequest;
+use Badzohreh\RolePermissions\Repositories\PermissionsRepo;
+use Badzohreh\RolePermissions\Repositories\RoleRepo;
 
 class RolePermissionsController extends Controller
 {
 
-    public function index()
+    private $RoleRepo;
+    private $PermissionRepo;
+
+    public function __construct(RoleRepo $RoleRepo,PermissionsRepo $permissionRepo) {
+        $this->RoleRepo = $RoleRepo;
+        $this->PermissionRepo= $permissionRepo;
+    }
+
+    public function index(PermissionsRepo $permissionRepo)
     {
-
-
-        $permissions = Permission::all();
-        $roles = Role::all();
+        $roles = $this->RoleRepo->all();
+        $permissions=$this->PermissionRepo->all();
         return view("RolePermissions::index", compact("roles","permissions"));
     }
+
+    public function store(RolePermissionStoreRequest $request)
+    {
+         $this->RoleRepo->store($request);
+         return  redirect(route("permissions.index"));
+    }
+
+    public function edit($id)
+    {
+        $role = $this->RoleRepo->findById($id);
+        $permissions = $this->PermissionRepo->all();
+       return view("RolePermissions::edit",compact("role","permissions"));
+    }
+
+    public function update(UpdateRolerequest $request,$roleId)
+    {
+        $this->RoleRepo->update($roleId,$request);
+        return redirect(route("permissions.index"));
+    }
+
+
+
 
 }
--- moduls/Badzohreh/RolePermissions/Resources/Views/edit.blade.php
+++ moduls/Badzohreh/RolePermissions/Resources/Views/edit.blade.php
@@ -5,34 +5,68 @@
         @include("Dashboard::layouts.header")
         <div class="breadcrumb">
             <ul>
-                <li><a href="index.html" title="پیشخوان">پیشخوان</a></li>
+                <li><a href="{{route("home")}}" title="پیشخوان">پیشخوان</a></li>
             </ul>
         </div>
         <div class="main-content">
             <div class="row no-gutters">
                 <div class="col-12 bg-white">
                     <p class="box__title">ایجاد دسته بندی جدید</p>
-                    <form action="{{ route("categories.update",$category->id) }}" method="post" class="padding-30">
+                    <form action="{{route("permissions.update",$role->id)}}" method="post" class="padding-30">
                         @method("patch")
                         @csrf
-                        <input type="text" name="title" required placeholder="نام دسته بندی"
-                               class="text" value="{{$category->title}}">
-                        <input type="text" name="slug" required placeholder="نام انگلیسی دسته بندی"
-                               class="text" value="{{$category->slug}}">
-                        <p class="box__title margin-bottom-15">انتخاب دسته پدر</p>
-                        <select name="parent_id">
-                            <option value="">ندارد</option>
-                            @foreach($categories as $categoryItem)
-                                <option value="{{$categoryItem->id}}" @if($categoryItem->title == $category->parent) selected @endif>{{$categoryItem->title}}</option>
-                            @endforeach
+                        <input type="text" name="name" required placeholder="نام دسته بندی"
+                               class="text" value="{{$role->name}}">
 
-                        </select>
-                        <button type="submit" class="btn btn-webamooz_net">اضافه کردن</button>
+
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                        <p class="box__title margin-bottom-15">انتخاب مجوز ها</p>
+
+
+
+                        @foreach($permissions as $permission)
+                            <label class="ui-checkbox">
+                                <input type="checkbox" name="permissions[{{$permission->name}}]"
+                                       class="sub-checkbox" data-id="1"
+                                       @if($role->hasPermissionTo($permission))
+                                               checked
+                                               @endif
+                                       value="{{$permission->name}}"
+                                >
+                                <span class="checkmark"></span>
+                                @lang($permission->name)
+                            </label>
+                        @endforeach
+
+
+                        @error('permissions')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        <br>
+
+
+                        @error('permission')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                        <br>
+
+
+
+
+                        <button type="submit" class="btn btn-webamooz_net">بروزرسانی</button>
                     </form>
                 </div>
 
             </div>
-            </div>
         </div>
+    </div>
     </div>
 @endsection
